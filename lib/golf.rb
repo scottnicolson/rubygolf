e7420a15d0c36818f17ca94e9fc0106695982a04
Tom Stuart <tom@experthuman.com>
2011-05-09 19:39:23 +0100

    Hole 4

--- lib/golf.rb
+++ lib/golf.rb
@@ -5,4 +5,7 @@ class Golf
   def self.hole3(n)
     n == 1 ? 1 : n * hole3(n-1)
   end
+  def self.hole4(m)
+    m.map { |n| n.sub(/man\((.*)/, 'hat(man(\1)').sub(/dog\((.*)\)/, 'dog(\1(bone))').sub(/cat/, 'dead') }
+  end
 end
