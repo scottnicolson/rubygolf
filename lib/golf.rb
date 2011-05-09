e1799dcc3c41e7d6b6030de6fd047d9b6109855b
Joel Chippindale <joel.chippindale@econsultancy.com>
2011-05-09 19:37:00 +0100

    Hole 7

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,12 @@
 class Golf
+  def self.hole7(a)
+    a.inject([[a.shift]]) { |r, n|
+      if n - 1 == r[-1][-1]
+        r[-1] << n
+        r
+      else
+        r << [n]
+      end
+      }.map { |a| a.size == 1 ? a[0].to_s : "#{a[0]}-#{a[-1]}" }
+  end
 end
